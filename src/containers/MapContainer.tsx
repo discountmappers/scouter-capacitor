537ee3a0a34aba959ebb14a9d0fd2c1d624bcada
khughes <kyle.hughes@disneystreaming.com>
2020-03-28 00:43:42 -0400

    location search prefilled, map will adjust based on location search

--- src/containers/MapContainer.tsx
+++ src/containers/MapContainer.tsx
@@ -1,23 +1,41 @@
-import React from "react";
-import './map.css'
+import React, { useEffect, useState } from "react";
 import GoogleMapReact from 'google-map-react';
-import { GOOGLE_API_KEY } from 'utils/env'
+import { GOOGLE_API_KEY } from 'utils/google'
+import { useGeoPosition } from "hooks/useGeoPosition";
+import { Grid } from "@material-ui/core";
+import { MapContainerActions } from "components/Deals";
+import './map.css'
+
 export const MapContainer = (props: any) => {
-    const manhattanCenter = {
-        lat: 40.7831,
-        lng: -73.9712
+    const { position, locationName, getLocation, searchByCustom } = useGeoPosition()
+    const [open, setMap] = useState(false)
+
+    // populate the text field & default center 
+    useEffect(() => {
+        getLocation()
+    }, [])
+
+    const toggleMap = () => {
+        setMap(!open)
     }
-    console.log(process.env.REACT_APP_GOOGLE_API_KEY)
     return (
-        // Important! Always set the container height explicitly for the map to show
-        <div className="mapContainer">
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: GOOGLE_API_KEY }}
-                defaultCenter={manhattanCenter}
-                defaultZoom={11}
-            >
-            </GoogleMapReact>
-        </div>
+        <Grid container justify="center" alignItems="center">
+            <Grid item className="actionGroup">
+                <MapContainerActions search={searchByCustom} toggleMap={toggleMap} location={locationName} />
+            </Grid>
+            {open && <Grid item xs={12}>
+                <div className="mapContainer">
+
+                    <GoogleMapReact
+                        key={position.lng}
+                        bootstrapURLKeys={{ key: GOOGLE_API_KEY }}
+                        defaultCenter={position}
+                        defaultZoom={11}
+                    >
+                    </GoogleMapReact>
+                </div>
+            </Grid>}
+        </Grid >
     )
 
 };
---
+++ src/utils/general.ts
@@ -0,0 +1,15 @@
+// explicit check for different types considered to be empty
+export const isEmpty = (value: any) => {
+    if (typeof value === 'string') {
+        value = value.trim()
+    }
+
+    return (
+        value === null ||
+        value === undefined ||
+        value === false ||
+        (value.constructor === Array && value.length === 0) ||
+        value === '' ||
+        (value.constructor === Object && Object.keys(value).length === 0)
+    )
+}
