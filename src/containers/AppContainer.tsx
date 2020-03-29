bce04c1b038273b49a90e407c678119f1644aee9
Ashley Nguyen <ashley.nguyen@disneystreaming.com>
2020-03-28 23:09:17 -0400

    css cleaning

--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -38,7 +38,7 @@ const useStyles = makeStyles({
     alignContent: 'center',
     display: 'flex',
     boxShadow: '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)',
-    paddingBottom: '15px'
+    paddingBottom: '15px',
   }
 });
 
