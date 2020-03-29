111dd6100523731100afb2e13e2530cac553fb38
Ashley Nguyen <ashley.nguyen@disneystreaming.com>
2020-03-28 20:05:25 -0400

    ajax

--- src/containers/SearchContainer.tsx
+++ src/containers/SearchContainer.tsx
@@ -1,35 +1,43 @@
-import React, { useEffect, useState, useContext } from "react";
-import GoogleMapReact from 'google-map-react';
-import { GOOGLE_API_KEY } from 'utils/google'
-import { useGeoPosition } from "hooks/useGeoPosition";
-import { Grid } from "@material-ui/core";
-import { MapContainerActions } from "components/Search";
+import React, {useContext, useEffect, useState} from "react";
+import {useGeoPosition} from "hooks/useGeoPosition";
+import {Grid} from "@material-ui/core";
+import {MapContainerActions} from "components/Search";
 import './map.css'
 import MapView from "components/Search/Map";
-import { HeaderContext } from "./AppContainer";
-import { SearchView } from "utils/general";
-import SearchFilter from "components/Search/searchFilter";
+import {HeaderContext} from "./AppContainer";
+import {SearchView} from "utils/general";
+import {SearchFilter} from "components/Search/searchFilter";
 
 type SearchContainerProps = {
     listView: boolean
 }
+
 export const SearchContainerContext = React.createContext({
     position: { lat: 0, lng: 0 },
     locationName: '',
     searchByCustom: (location: string) => new Promise<void>(resolve => { }),
+    filterResults: [],
+    setResults: (value: any) => {}
 })
 
 export const SearchContainer = (props: SearchContainerProps) => {
     const { searchView, setSearchView } = useContext(HeaderContext)
+    const [filterResults, setFilterResults] = useState([])
     const { position, locationName, getLocation, searchByCustom } = useGeoPosition()
+    console.log("search container: ", filterResults)
 
     // populate the text field & default center
     useEffect(() => {
         getLocation()
     }, [])
 
+    const setResults = (value: any) => {
+        setFilterResults(value)
+        setSearchView(SearchView.LIST)
+    }
+
     return (
-        <SearchContainerContext.Provider value={{ position, locationName, searchByCustom }}>
+        <SearchContainerContext.Provider value={{ position, locationName, searchByCustom, filterResults, setResults }}>
             <Grid container justify="center" alignItems="center">
                 <Grid item xs={10} sm={6}>
                     <div className="actionGroup">
