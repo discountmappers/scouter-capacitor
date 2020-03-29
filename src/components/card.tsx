86d8915388eb42c367f70cd1fbd20331a464cf86
khughes <kyle.hughes@disneystreaming.com>
2020-03-28 23:49:51 -0400

    make custom markers on the map, default the context to the mock data

---
+++ src/components/card.tsx
@@ -0,0 +1,49 @@
+import { makeStyles, CardContent, Typography, Card } from "@material-ui/core";
+import React, { useContext } from "react";
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        alignItems: 'center',
+        boxShadow: '1px 1px 1px 1px #888888'
+
+    },
+    details: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    content: {
+        flex: '1 0 auto',
+    },
+    img: {
+        width: 60,
+        height: 60,
+        padding: '10px 10px 10px 10px'
+    },
+
+
+});
+
+type CardProps = {
+    result: any
+}
+const CustomCard = (props: CardProps) => {
+    const { result } = props
+    const classes = useStyles();
+    return (<Card className={classes.root}>
+        <img className={classes.img} src="https://maps.gstatic.com/mapfiles/place_api/icons/cafe-71.png" />
+
+        <div className={classes.details}>
+            <CardContent className={classes.content}>
+                <Typography variant="body2">
+                    {result.dealDesc}
+                </Typography>
+                <Typography variant="subtitle2" color="textSecondary">
+                    {result.name}
+                </Typography>
+            </CardContent>
+        </div>
+    </Card>)
+}
+
+export default CustomCard
